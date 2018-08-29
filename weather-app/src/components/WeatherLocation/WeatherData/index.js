4e966a2a6ab7b9d7f11e37a6444a1cc095bd80f7
Angel Peralta <angperalta01@gmail.com>
2018-08-29 17:14:30 -0500

    The last chage using the components properties

--- weather-app/src/components/WeatherLocation/WeatherData/index.js
+++ weather-app/src/components/WeatherLocation/WeatherData/index.js
@@ -2,26 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import WeatherTemperature from './WeatherTemperature';
-
-import { SNOW } from './../../../constants/weathers';
+// import {SNOW} from './../../../constants/weathers';
 import './styles.css';
 
 const WeatherData = ({data}) => {
-	//proseguimos a hacer un destructuring e spor ese motivo que esto luce asi temperature = {temperature}
-	const { temperature, weatherState, humidity, wind } = data;
-	return (
-    <div className='weatherDataCont' >
-        <WeatherTemperature temperature={temperature} weatherState={weatherState}/>
-        <WeatherExtraInfo humidity={humidity} wind={wind}/>
-    </div>);
+  //proseguimos a hacer un destructuring e spor ese motivo que esto luce asi temperature = {temperature}
+  const {temperature, weatherState, humidity, wind} = data;
+  return (<div className='weatherDataCont'>
+    <WeatherTemperature temperature={temperature} weatherState={weatherState}/>
+    <WeatherExtraInfo humidity={humidity} wind={wind}/>
+  </div>);
 };
 WeatherData.propTypes = {
-	data: PropTypes.shape({
-			temperature: PropTypes.number.isRequired,
-			weatherState: PropTypes.string.isRequired,
-			humidity: PropTypes.number.isRequired,
-			wind: PropTypes.string.isRequired,
-	}),
+  data: PropTypes.shape({temperature: PropTypes.number.isRequired, weatherState: PropTypes.string.isRequired, humidity: PropTypes.number.isRequired, wind: PropTypes.string.isRequired})
 };
 export default WeatherData;
 // Crea un programa que imprima en consola 3 tipos de autos y sus caracteristicas.
