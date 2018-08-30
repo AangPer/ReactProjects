f43fb6c25bf47def15410b79a64e0ffd60ad5e6e
Angel Peralta <angperalta01@gmail.com>
2018-08-30 18:49:23 -0500

    usin material-ui library

--- weather-app/src/components/WeatherLocation/index.js
+++ weather-app/src/components/WeatherLocation/index.js
@@ -3,20 +3,14 @@
 import React, {Component} from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
+import CircularProgress from 'material-ui/CircularProgress';
 import transformWeather from './../../services/transformWeather';
-import { SUN } from './../../constants/weathers';
 import './styles.css';
 
 const location = 'Villahermosa,mx';
 const apiKey = '84b353d0a9367047b3ee3fc18e4afa07';
-const apiWeather =  `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${apiKey}`;
+const apiWeather = `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${apiKey}`;
 
-const data = {
-	temperature: 20,
-	weatherState: SUN,
-	humidity: 10,
-	wind:'10 m/s',
-};
 // This is our functional component as we can see it is a function that returned the rendering of our component
 //
 // const WeatherLocation = () => (
@@ -26,38 +20,43 @@ const data = {
 //     </div>
 // );
 
-// This extend from the 3th line we are extracting just one component 
+// This extend from the 3th line we are extracting just one component
 class WeatherLocation extends Component {
 
-	constructor(){
-		super();
-		this.state = {
-			city: 'Villahermosa',
-			data: data
-	};
-}
+  constructor() {
+    super();
+    this.state = {
+      city: 'Villahermosa',
+      data: null
+    };
+    console.log("constructor");
+  }
 
-	handleUpdateClick = () => {
-	fetch(apiWeather).then(data => {
-		console.log(data);
-		return data.json();
-	}).then(weather_data => {
-		debugger;
-		const data = transformWeather(weather_data);
-		this.setState({data});
-	});
-		console.log("Actualizado");
-		}
+  handleUpdateClick = () => {
+    fetch(apiWeather).then(data => {
+      console.log(data);
+      return data.json();
+    }).then(weather_data => {
+      const data = transformWeather(weather_data);
+      this.setState({data});
+    });
+    // console.log("Actualizado");
+  }
 
-	render = () => {
-		const {city,data}=this.state;
-		return(
-	    <div className="weatherLocationCont">
-	        <Location city={city}></Location>
-	        <WeatherData data={data}></WeatherData>
-	        <br/>
-	        <button onClick={this.handleUpdateClick}>Actualizar</button>
-	    </div>);
-	};
+  componentWillMount() {
+    this.handleUpdateClick();
+  }
+
+  render = () => {
+    console.log("render");
+    const {city, data} = this.state;
+    return (
+    <div className='weatherLocationCont'>
+      <Location city={city}/>
+      {data ? <WeatherData data={data}/> :
+      <CircularProgress size={60} thickness={7} />}
+      <br/>
+    </div>);
+  };
 };
 export default WeatherLocation;
