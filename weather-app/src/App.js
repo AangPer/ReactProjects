44b7c0226c3478c4e8a6b4d0fa1f68435c0a65b1
Angel Peralta <angperalta01@gmail.com>
2018-09-04 19:43:31 -0500

    Ciclos de actualizacion de state y render puedes ver una explicacion mas detallada en el video numero 6 de la carpeta 11. Mas herramientas y ES6

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -25,11 +25,14 @@ class App extends Component {
     //this.state= just can be used in the constructor of the component in another place we need to use set.state
   }
   handleSelectedLocation = city => {
+    this.setState({city});
+    //Mira aqui city es una propiedad y a la vez el parametro que vamos a pasar por ese motivo no ponemos ({city:city}) mejor lo simplificamos porque es ECMA6
     console.log(`handleSelectedLocation ${city}`);
   }
   render() {
     const { city } = this.state;
     //destructuring for using in the line 49 beacouse in otherwise city={this.state.city}
+    // Inicialmente se genera nuestro componente app.js con el valor inicaial que le damos al state le establecemos city al valor nueva ciudad L24 una vez que nuestro location list genera el evento L47 y lo pasa como parametro L27, establecemos un nuevo estado L28 oscea establecemos un nuevo valor para la propiedad city L24 lo igualamos con los parametros en la L33
     return (
 
     <MuiThemeProvider>
